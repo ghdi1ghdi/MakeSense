f2a9745a103abf4bdb60870cc5534353e687a26e
Lee Jeong Ho <73730434+ghdi1ghdi@users.noreply.github.com>
2023-12-02 17:17:37 +0900

    Initial commit

---
+++ News_crawler/newsFlashTitleCrawler.py
@@ -0,0 +1,37 @@
+import requests
+from bs4 import BeautifulSoup
+import json
+
+def crawler(soup):
+    #print(soup)        >> 작동 확인 완료
+    div = soup.find("div", class_ = "list_body")
+
+    result = []
+    for a in div.find_all("a"):      #find_all의 반환값 형태는 리스트
+        result.append(a.get_text())
+    return result
+
+def json_maker(result):
+    with open("newsFlashTitle.json", "w", encoding = "utf-8") as f:
+        json.dump(result, f, ensure_ascii = False, indent = '\t')
+
+def main():
+    url = "https://news.naver.com/main/list.naver?mode=LPOD&mid=sec&sid1=001&sid2=140&oid=001&isYeonhapFlash=Y"
+
+    #네이버 연합뉴스 속보 제목타이틀 가져오기
+    custom_header = {
+        'referer' : 'https://www.naver.com/',
+        'user-agent' : 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.103 Safari/537.36'
+    }
+
+    url = "https://news.naver.com/main/list.nhn?mode=LPOD&mid=sec&sid1=001&sid2=140&oid=001&isYeonhapFlash=Y"
+    req = requests.get(url, headers = custom_header)
+    #print(req.status_code)
+
+    soup = BeautifulSoup(req.text, "html.parser")
+    result = crawler(soup)
+    print(result)
+    json_maker(result)
+    
+if __name__ == "__main__":
+    main()
---
+++ News_crawler/newsTodayTitleCrawler.py
@@ -0,0 +1,44 @@
+import requests
+from bs4 import BeautifulSoup
+from datetime import datetime
+import json
+
+def crawler(soup):
+    result = []
+    for h2 in soup.find_all("h2", class_="tit"):
+        result.append(h2.get_text())
+    return result
+
+def json_maker(result):
+    with open("newsTodayTitle.json", "w", encoding="utf-8") as f:
+        json.dump(result, f, ensure_ascii=False, indent='\t')
+
+def main():
+    custom_header = {
+        'referer': 'https://www.nate.com/',
+        'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.103 Safari/537.36'
+    }
+
+    base_url = "https://news.nate.com/recent?mid=n0100&type=c"
+    
+    today = datetime.today().strftime('%Y%m%d')
+
+    
+    base_date_url = base_url + "&date=" + str(today)
+    
+    print(base_date_url)
+    
+    all_results = []
+    
+    for i in range(19):
+        url = base_date_url + "&page=" + str(i+1)
+        req = requests.get(url, headers=custom_header)
+        soup = BeautifulSoup(req.text, "html.parser")
+        result = crawler(soup)
+        all_results.extend(result)
+        print(result)
+        
+    json_maker(all_results)
+
+if __name__ == "__main__":
+    main()
